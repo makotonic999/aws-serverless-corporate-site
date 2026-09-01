5bce3a7c262a666fdbb10d62a60fea16603bcc7a
makotonic999 <makotonic999@gmail.com>
2026-09-01 06:28:00 +0000

    test: check oidc

--- terraform/environments/dev/iam_github_actions.tf
+++ terraform/environments/dev/iam_github_actions.tf
@@ -72,7 +72,7 @@ resource "aws_iam_role" "github_actions_deploy" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:makotonic999/aws-serverless-corporate-site:*"
+            "token.actions.githubusercontent.com:sub" = "repo:makotonic999/aws-serverless-corporate-site:ref:refs/heads/feature/oidc-test"
           }
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
